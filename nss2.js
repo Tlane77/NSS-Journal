ab00f92111619a79244340e2350b98c1e9483b81
Tasha Lane <designtmg77@gmail.com>
2020-06-01 07:02:05 -0500

    added variable

---
+++ nss2.js
@@ -0,0 +1,73 @@
+const JournalentryOne = {
+    date: "Date of Entry",
+    concepts: "Concepts Covered",
+    content: "Info Covered",
+    mood: "Mood of the Day",
+
+}
+
+const JournalentryOne = {
+    date: "Date of Entry",
+    concepts: "Concepts Covered",
+    content: "Info Covered",
+    mood: "Mood of the Day",
+
+}
+
+const JournalentryOne = {
+    date: "Date of Entry",
+    concepts: "Concepts Covered",
+    content: "Info Covered",
+    mood: "Mood of the Day",
+
+}
+
+const JournalentryTwo = {
+    date: "Date of Entry",
+    concepts: "Concepts Covered",
+    content: "Info Covered",
+    mood: "Mood of the Day",
+
+}
+
+const JournalentryThree = {
+    date: "Date of Entry",
+    concepts: "Concepts Covered",
+    content: "Info Covered",
+    mood: "Mood of the Day",
+
+}
+
+const JournalentryFour = {
+    date: "Date of Entry",
+    concepts: "Concepts Covered",
+    content: "Info Covered",
+    mood: "Mood of the Day",
+
+}
+
+const JournalentryFive = {
+    date: "Date of Entry",
+    concepts: "Concepts Covered",
+    content: "Info Covered",
+    mood: "Mood of the Day",
+
+}
+
+const JournalentrySix = {
+    date: "Date of Entry",
+    concepts: "Concepts Covered",
+    content: "Info Covered",
+    mood: "Mood of the Day",
+
+}
+
+const JournalentrySeven = {
+    date: "Date of Entry",
+    concepts: "Concepts Covered",
+    content: "Info Covered",
+    mood: "Mood of the Day",
+
+}
+
+var journalEntries = []
